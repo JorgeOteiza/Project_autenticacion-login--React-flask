97ed4ab2980b04a1a8d2d38ecd2551e19d5f5ea5
Jorge Oteiza <oteiza.jor@gmail.com>
2024-08-28 22:53:53 +0000

    Creación de botón y ruta de acceso: "Create account"; definición de estilos

---
+++ src/front/js/component/LogIn.jsx
@@ -0,0 +1,155 @@
+import React, { useState, useContext, useEffect } from "react";
+import { Context } from "../store/appContext";
+import { Link, useNavigate } from "react-router-dom";
+
+const LogIn = () => {
+  const { store, actions } = useContext(Context);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const token = localStorage.getItem("token");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    actions.login(email, password).then((res) => {
+      navigate("/");
+    });
+
+    setTimeout(() => {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Datos erróneos o usuario inexiste!",
+        });
+        setEmail("");
+        setPassword("");
+      } else {
+        Swal.fire({
+          icon: "success",
+          title: "Has accedido correctamente!",
+        });
+        setTimeout(() => {
+          window.location.reload(false);
+        }, 1500);
+      }
+    }, 2000);
+  };
+
+  return (
+    <>
+      <div
+        className="modal fade modal-log-in"
+        tabIndex="-1"
+        id="modalLogin"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-lg">
+          <div className="modal-content modal-content-log-in">
+            <div className="modal-header">
+              <h5 className="modal-title">Acceso</h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-login-body mb-3"></div>
+
+            <form onSubmit={handleLogin}>
+              {/*  input email */}
+              <div className="input-group-login">
+                <div className="input-field pt-4">
+                  <span className="far fa-user p-2"></span>
+                  <input
+                    value={email}
+                    id="email"
+                    type="text"
+                    placeholder="Correo usuario"
+                    className="input-field-login"
+                    required
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+
+                {/* input contraseña */}
+                <div className="form-group-login py-1 pb-2">
+                  <div className="input-field">
+                    <span className="fas fa-lock p-2"></span>
+                    <input
+                      value={password}
+                      id="password"
+                      type="password"
+                      placeholder="Contraseña"
+                      className="input-field-login"
+                      required
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+                </div>
+              </div>
+
+              {/*  boton ingresar */}
+              <div
+                className="boton-login col-12 mx-auto m-3"
+                data-bs-dismiss="modal"
+              >
+                <button
+                  type="submit"
+                  className="btn btn-dark data-bs-dismiss w-100"
+                >
+                  Ingresar
+                </button>
+              </div>
+            </form>
+
+            <div>
+              {/*   boton google */}
+              <div className="boton-login col-12 mx-auto mb-3">
+                <button
+                  className="btn btn-danger w-100"
+                  type="button"
+                  value="Login"
+                  disabled
+                >
+                  {" "}
+                  (Próximamente) Continúa con{" "}
+                  <i className="fab fa-google me-2"></i>
+                </button>
+              </div>
+            </div>
+
+            {/*  olvide mi contraseña */}
+            <button
+              className="olvide-mi-contraseña mb-2"
+              data-bs-toggle="modal"
+              data-bs-target="#modalRestaurarContraseña"
+              data-bs-dismiss="modal"
+            >
+              ¿Olvidaste tu contraseña?
+            </button>
+
+            {/* link para registro */}
+            <div className="crear-cuenta-login" data-bs-dismiss="modal">
+              <span>
+                <Link
+                  to="/registro"
+                  style={{ textDecoration: "none", color: "inherit" }}
+                >
+                  Crear una cuenta
+                </Link>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+console.log(process.env.VARIABLE_NAME);
+export default LogIn;
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,24 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import { useNavigate } from "react-router-dom";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
+  const navigate = useNavigate();
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  const handleLoginRedirect = () => {
+    navigate("/login");
+  };
+
+  return (
+    <div className="text-center my-5 py-5">
+      <button
+        className="stylesAcess border-info p-3 my-5 rounded-pill"
+        onClick={handleLoginRedirect}
+      >
+        <h5>Create account</h5>
+      </button>
+    </div>
+  );
 };
