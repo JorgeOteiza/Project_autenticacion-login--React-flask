a7e1e93d888673b7b83ef5b6aa7288af7e7763e2
Jorge Oteiza <oteiza.jor@gmail.com>
2024-08-29 04:23:49 +0000

    cambio de estilos navbar y footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-  <footer className="footer bg-black opacity-50 text-center"></footer>
+  <footer className="footer py-1 m-5 bg-black opacity-50 text-center rounded-circle"></footer>
 );
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <nav className="navbar navbar-light">
+    <nav className="navbar navbar-light bg-black opacity-50 py-1 m-5 rounded-circle">
       <div className="container text-adjust-center"></div>
     </nav>
   );
