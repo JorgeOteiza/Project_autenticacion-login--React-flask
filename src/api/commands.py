d2d54b348156fdf23ee5145b38c7f0a7c6e6a09e
Jorge Oteiza <oteiza.jor@gmail.com>
2024-09-10 19:37:44 +0000

    [solución duplicado 5 users] se modifica commands.py para evitar emails duplicados si el usuario ya existe.

--- src/api/commands.py
+++ src/api/commands.py
@@ -14,19 +14,26 @@ def setup_commands(app):
     by typing: $ flask insert-test-users 5
     Note: 5 is the number of users to add
     """
-    @app.cli.command("insert-test-users") # name of our command
-    @click.argument("count") # argument of out command
+    @app.cli.command("insert-test-users")
+    @click.argument("count")
     def insert_test_users(count):
         print("Creating test users")
         for x in range(1, int(count) + 1):
+            email = f"test_user{x}@test.com"
+            # Verificar si el usuario ya existe
+            existing_user = User.query.filter_by(email=email).first()
+            if existing_user:
+                print(f"User with email {email} already exists. Skipping.")
+                continue
+            
             user = User()
-            user.email = "test_user" + str(x) + "@test.com"
+            user.email = email
             user.set_password("123456")
             user.is_active = True
             db.session.add(user)
             db.session.commit()
             print(f"User: {user.email} created.")
-
+        
         print("All test users created")
 
     @app.cli.command("insert-test-data")
